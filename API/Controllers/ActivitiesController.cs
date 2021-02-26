791a6a0cc542e578aa0e916afbddf1c5a9b46bce
aaron <a@a.com>
2021-02-26 16:55:10 +0000

    Initial commit

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{url}")]
+        public async Task<ActionResult<Activity>> GetActivity(string url)
+        {
+            return await _context.Activities.Where(b => b.Url == url).FirstOrDefaultAsync();
+        }
+    }
+}
